4d1691127b7931094b92b8157f559820f2fa6697
Karolka87 <115976221+Karolka87@users.noreply.github.com>
2023-02-27 13:51:10 +0100

    leetcode15

---
+++ src/ThreeSum.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class ThreeSum {
+    public static void main(String[] args) {
+        int[] nums = {-1, 0, 1, 2, -1, -4};
+        List<List<Integer>> list = new ArrayList<List<Integer>>();
+        Arrays.sort(nums);
+        for(int i = 0; i < nums.length-2; i++) {
+            if(i > 0 && (nums[i] == nums[i-1])) continue; // avoid duplicates
+            for(int j = i+1, k = nums.length-1; j<k;) {
+                if(nums[i] + nums[j] + nums[k] == 0) {
+                    list.add(Arrays.asList(nums[i],nums[j],nums[k]));
+                    j++;k--;
+                    while((j < k) && (nums[j] == nums[j-1]))j++;// avoid duplicates
+                    while((j < k) && (nums[k] == nums[k+1]))k--;// avoid duplicates
+                }else if(nums[i] + nums[j] + nums[k] > 0) k--;
+                else j++;
+            }
+        }
+        System.out.println(list);;
+    }
+    }
